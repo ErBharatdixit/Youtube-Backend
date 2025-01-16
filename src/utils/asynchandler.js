8e823f9a0a3a62ac298b1c375635f0b68dea48eb
Bharat Dixit <bharat010703@gmail.com>
2025-01-16 20:59:26 +0530

    Error Handling

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,25 @@
+/************************* USING PROMISES*****************************/
+
+const asyncHandler =(requesHandler) =>{
+      (req,res,next) =>{
+            Promise.resolve(requesHandler(req, res, next)).catch((err) => next(err))
+      }
+}
+
+
+
+
+export {asyncHandler}
+
+/*********************USING TRY CATCH************************* */
+// const asyncHandler = (fn) =>async (req,res,next) => {
+//       try{
+//            await fn(req,res,next)
+//       } catch(error){
+//             res.status(err.code || 500).json({
+//                   success: false,
+//                   message: err.message
+//             })
+//       }
+// }
+
